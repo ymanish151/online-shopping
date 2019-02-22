5606d6abf0fb912643914c48eebc9198fbac4ee4
manish <ymanish151@gmail.com>
2019-02-22 18:15:00 +0530

    Completed the theme and other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -7,12 +7,28 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
-	@RequestMapping(value = {"/","/home","/index"})
+	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
-		ModelAndView mv =new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring web mvc");
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
-	
-	
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+
 }
